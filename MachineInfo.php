bf68e22d9d5f2ac0dbcb9877dad4b36c80f3db7f
ImOverlord <lfihk-thibault@users.noreply.github.com>
2016-09-12 17:20:38 +0800

    Get Machine Info Script
    Simple script to get Machine Info with PHP

---
+++ MachineInfo.php
@@ -0,0 +1,84 @@
+<?php
+// Gets user agent which is gonna be used for the following fucntions.
+$user_agent     =   $_SERVER['HTTP_USER_AGENT'];
+
+function getOS() { 
+
+    global $user_agent;
+
+    $os_platform    =   "Unknown OS Platform";
+
+    $os_array       =   array(
+                            '/windows nt 10/i'     =>  'Windows 10',
+                            '/windows nt 6.3/i'     =>  'Windows 8.1',
+                            '/windows nt 6.2/i'     =>  'Windows 8',
+                            '/windows nt 6.1/i'     =>  'Windows 7',
+                            '/windows nt 6.0/i'     =>  'Windows Vista',
+                            '/windows nt 5.2/i'     =>  'Windows Server 2003/XP x64',
+                            '/windows nt 5.1/i'     =>  'Windows XP',
+                            '/windows xp/i'         =>  'Windows XP',
+                            '/windows nt 5.0/i'     =>  'Windows 2000',
+                            '/windows me/i'         =>  'Windows ME',
+                            '/win98/i'              =>  'Windows 98',
+                            '/win95/i'              =>  'Windows 95',
+                            '/win16/i'              =>  'Windows 3.11',
+                            '/macintosh|mac os x/i' =>  'Mac OS X',
+                            '/mac_powerpc/i'        =>  'Mac OS 9',
+                            '/linux/i'              =>  'Linux',
+                            '/ubuntu/i'             =>  'Ubuntu',
+                            '/iphone/i'             =>  'iPhone',
+                            '/ipod/i'               =>  'iPod',
+                            '/ipad/i'               =>  'iPad',
+                            '/android/i'            =>  'Android',
+                            '/blackberry/i'         =>  'BlackBerry',
+                            '/webos/i'              =>  'Mobile'
+                        );
+	//loops through os array and check of the user_agant has a match
+    foreach ($os_array as $regex => $value) { 
+
+        if (preg_match($regex, $user_agent)) {
+            $os_platform    =   $value;
+        }
+
+    }   
+
+    return $os_platform;
+
+}
+
+function getBrowser() {
+
+    global $user_agent;
+
+    $browser        =   "Unknown Browser";
+
+    $browser_list  =   array(
+                            '/msie/i'       =>  'Internet Explorer',
+                            '/firefox/i'    =>  'Firefox',
+                            '/safari/i'     =>  'Safari',
+                            '/chrome/i'     =>  'Chrome',
+                            '/edge/i'       =>  'Edge',
+                            '/opera/i'      =>  'Opera',
+                            '/netscape/i'   =>  'Netscape',
+                            '/maxthon/i'    =>  'Maxthon',
+                            '/konqueror/i'  =>  'Konqueror',
+                            '/mobile/i'     =>  'Handheld Browser'
+                        );
+
+    foreach ($browser_list as $regex => $value) { 
+
+        if (preg_match($regex, $user_agent)) {
+            $browser    =   $value;
+        }
+
+    }
+
+    return $browser;
+
+}
+
+$IP = $_SERVER['REMOTE_ADDR'];
+$user_os        =   getOS();
+$user_browser   =   getBrowser();
+
+?>
